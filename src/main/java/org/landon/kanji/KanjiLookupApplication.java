8123884bbb9e61802d77f5256926981cbd24afb3
LandonHM <pureskillezsandwitch@gmail.com>
2022-09-03 01:51:48 -0500

    Added metadata and robots.txt for seo

--- src/main/java/org/landon/kanji/KanjiLookupApplication.java
+++ src/main/java/org/landon/kanji/KanjiLookupApplication.java
@@ -2,7 +2,12 @@ package org.landon.kanji;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
 
 @SpringBootApplication
 @Controller
@@ -10,6 +15,12 @@ public class KanjiLookupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KanjiLookupApplication.class, args);
+	}
+
+	@GetMapping(value = "robots.txt", produces = MediaType.TEXT_PLAIN_VALUE)
+	public @ResponseBody String getRobotsTxt() {
+		return "User-agent: *\n" +
+				"Disallow:\n";
 	}
 
 }

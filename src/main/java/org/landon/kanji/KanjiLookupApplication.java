f7b22a198984b5887d82b620c16663231c3596e7
LandonHM <pureskillezsandwitch@gmail.com>
2022-10-27 00:26:37 -0500

    Add port 80 to redirect to 443 for https always

--- src/main/java/org/landon/kanji/KanjiLookupApplication.java
+++ src/main/java/org/landon/kanji/KanjiLookupApplication.java
@@ -1,13 +1,27 @@
 package org.landon.kanji;
 
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.ServerConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.ConfigurableJettyWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.webapp.AbstractConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 
 import java.awt.*;
+import java.util.Collections;
 
 @SpringBootApplication
 @Controller
@@ -23,4 +37,45 @@ public class KanjiLookupApplication {
 				"Disallow:\n";
 	}
 
+}
+
+@Configuration
+class WebServerFactoryCustomizerConfig implements WebServerFactoryCustomizer<ConfigurableJettyWebServerFactory> {
+	@Override
+	public void customize(ConfigurableJettyWebServerFactory factory) {
+		((JettyServletWebServerFactory)factory).setConfigurations(
+				Collections.singleton(new HttpToHttpsJettyConfig())
+		);
+
+		factory.addServerCustomizers(
+				server -> {
+					HttpConfiguration httpConfiguration = new HttpConfiguration();
+					httpConfiguration.setSecurePort(8443);
+					httpConfiguration.setSecureScheme("https");
+
+					ServerConnector connector = new ServerConnector(server);
+					connector.addConnectionFactory(new HttpConnectionFactory(httpConfiguration));
+					connector.setPort(8080);
+
+					server.addConnector(connector);
+				}
+		);
+	}
+}
+
+class HttpToHttpsJettyConfig extends AbstractConfiguration {
+	@Override
+	public void configure(WebAppContext context) throws Exception {
+		Constraint constraint = new Constraint();
+		constraint.setDataConstraint(Constraint.DC_CONFIDENTIAL);
+
+		ConstraintMapping mapping = new ConstraintMapping();
+		mapping.setPathSpec("/*");
+		mapping.setConstraint(constraint);
+
+		ConstraintSecurityHandler handler = new ConstraintSecurityHandler();
+		handler.addConstraintMapping(mapping);
+
+		context.setSecurityHandler(handler);
+	}
 }
